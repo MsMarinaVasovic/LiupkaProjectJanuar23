b249caecc9a30badf239ac65bed94cd691a71803
MsMarinaVasovic <129836995+MsMarinaVasovic@users.noreply.github.com>
2023-04-27 15:59:21 +0200

    Update HomePage.java

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -7,19 +7,14 @@ public class HomePage extends BasePage{
     //web elements
     @FindBy(xpath = "//div[@class='box-header-nav']//div[@class='block-nav-categori']//div[@class='block-title']")
     WebElement burgerMenuProdavnica;
-
     @FindBy (xpath = "//a/span[@class='user-icon']")
     WebElement userIcon;
-
     @FindBy (xpath = "//a[@class='banner-youtube']")
     WebElement youtubeBanner;
-
     @FindBy (xpath = "//div[@class='col-md-3 col-sm-3 col-xs-12 nav-right']//span[@class='cart-icon']")
     WebElement shoppingCart;
-
     @FindBy (xpath = "//div[@class='col-md-3 col-sm-3 col-xs-12 nav-right']//span[@class='counter-number']")
     WebElement numberOfItemsInShoppingCart;
-
     @FindBy (xpath = "//div[@class='content-right p1']//div[contains(text(),'KURSEVI')]")
     WebElement footerLinkKursevi;
     @FindBy (xpath = "//div[@class='content-right p1']//div[contains(text(),'NAČIN PLAĆANJA')]")
@@ -27,9 +22,7 @@ public class HomePage extends BasePage{
     @FindBy (xpath = "//div[@class='content-right p1']//div[contains(text(),'DOSTAVA ')]")
     WebElement footerLinkDostava;
 
-
     //constructor
-
     public HomePage (ChromeDriver driver){
         super(driver);
         driver.get(Strings.HOME_PAGE_URL);
@@ -42,14 +35,12 @@ public class HomePage extends BasePage{
         assert isElementPresent(burgerMenuProdavnica) : "Burger menu 'Prodavnica' is not present";
         burgerMenuProdavnica.click();
     }
-
     public void clickOnUserIcon (){
         print("clickOnUserIcon");
         waitForElement(userIcon);
         assert isElementPresent(userIcon) : "Element User Icon is not present";
         userIcon.click();
     }
-
     public void clickOnYoutubeBanner (){
         print("clickOnYoutubeBanner");
         waitForElement(youtubeBanner);
@@ -81,5 +72,4 @@ public class HomePage extends BasePage{
         assert isElementPresent(footerLinkDostava) : "Element from footer DOSTAVA is not present";
         footerLinkDostava.click();
     }
-
 }
