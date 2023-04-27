94e3ad7c76fc331bd66a6ee16f5df56d49e757dd
MsMarinaVasovic <129836995+MsMarinaVasovic@users.noreply.github.com>
2023-04-27 15:59:26 +0200

    Update Strings.java

--- src/main/java/Strings.java
+++ src/main/java/Strings.java
@@ -1,21 +1,14 @@
 public class Strings {
 
     public static final String HOME_PAGE_URL = "https://www.liupka.com/";
-
     public static final String LOGIN_AND_REGISTRATION_PAGE_URL = "https://www.liupka.com/rs/?action=account";
-
     public static final String USER_ACCOUNT_PAGE_URL = "https://www.liupka.com/rs/?action=account";
-
     public static final String YOUTUBE_BANNER_URL = "https://www.youtube.com/channel/UCq_RnjTA4nz-3dJ5Ui5sbyw";
-
     public static final String KURSEVI_I_RADIONICE_URL ="https://www.liupka.com/rs/kursevi-i-radionice";
     public static final String NACIN_PLACANJA_URL ="https://www.liupka.com/rs/meni/kupovina/nacini-placanja-2766";
     public static final String DOSTAVA_URL ="https://www.liupka.com/rs/meni/kupovina/dostava-do-vas";
-
     public static final String SORT_BY_PRICE_ASC = "Najnižoj ceni";
-
     public static final int NUMBER_OF_SORTING_OPTIONS = 6;
-
     public static final String SORT_OPTION_1 = "Standardnom prikazu";
     public static final String SORT_OPTION_2 = "Najnižoj ceni";
     public static final String SORT_OPTION_3 = "Najvišoj ceni";
@@ -25,6 +18,5 @@ public class Strings {
     public static final String VALID_USER_NAME = "marinavasovictestmail@gmail.com";
     public static final String VALID_PASSWORD = "testpass123";
     public static final String INVALID_PASSWORD = "wrongpassword";
-
     public static final String ERROR_MESSAGE = "Pogrešna lozinka!";
 }
