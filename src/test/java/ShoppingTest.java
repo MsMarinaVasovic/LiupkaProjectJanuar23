bd6d7f7a25e63e5e37dbd5ed22d404c6ac386860
MsMarinaVasovic <129836995+MsMarinaVasovic@users.noreply.github.com>
2023-05-04 11:24:10 +0200

    Update ShoppingTest.java

--- src/test/java/ShoppingTest.java
+++ src/test/java/ShoppingTest.java
@@ -1,5 +1,11 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
 
 public class ShoppingTest extends BaseTest {
     /**
@@ -33,6 +39,54 @@ public class ShoppingTest extends BaseTest {
             driver.navigate().refresh();
             String currentlNumberOfItemsInCart = homePage.getNumberOfItemsInShoppingCart();
             assert currentlNumberOfItemsInCart.equals("1") : "Wrong number of items in cart. Expected 1, actual: " + currentlNumberOfItemsInCart;
+
+        }finally {
+            driver.quit();
+        }
+    }
+
+    /**
+     * Testing adding one item to the shopping cart
+     * Steps:
+     * 1.Go to home page
+     * 2.Click on burger menu PRODAVNICA
+     * 3.Click on option Sve kategorije
+     * 4.Click on option Kursevi i radionice
+     * 5. Click on button U KORPU on the selected product
+     * 6.Click on field for quantity enter (modal window)
+     *
+     * Expected results:
+     * Verify that icon on shopping cart shows 1
+     */
+    @Test
+    public void testAddTwoItemsInShoppingCart() {
+        print("Testing adding one item to the shopping cart");
+        ChromeDriver driver = openChromeDriver();
+        try {
+            print("1.Go to home page");
+            HomePage homePage = new HomePage(driver);
+            homePage.allowCoociesButton.click();
+            print("2.Click on burger menu PRODAVNICA");
+            homePage.clickOnBurgerMenuProdavnica();
+            StorePages storePages = new StorePages(driver);
+            print("3.Click on option Sve kategorije");
+            storePages.clickOnSveKategorije();
+            print("4.Click on option Kursevi i radionice");
+            storePages.clickOnKurseviIRadionice();
+            print("5. Click on button U KORPU on the selected product");
+            storePages.clickOnUKorpuSTORITVEButton();
+            print("6.Click on field for quantity enter");
+            WebDriverWait wait = new WebDriverWait(driver, 30);
+            WebElement kolicinaFieldFromModal = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='addItemQuantity']")));
+            Actions action=new Actions(driver);
+            action.moveToElement(kolicinaFieldFromModal).click().build().perform();
+            kolicinaFieldFromModal.sendKeys(Keys.BACK_SPACE);
+            kolicinaFieldFromModal.sendKeys("2");
+            action.moveToElement(storePages.closeButtonFromModalWindow).click().build().perform();
+            driver.navigate().refresh();
+
+            String currentlNumberOfItemsInCart = homePage.getNumberOfItemsInShoppingCart();
+            assert currentlNumberOfItemsInCart.equals("2") : "Wrong number of items in cart. Expected 2, actual: " + currentlNumberOfItemsInCart;
 
         }finally {
             driver.quit();
