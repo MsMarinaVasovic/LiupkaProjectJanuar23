5bb5ed5fc2ff2f4c618f83b98f695a9ba482b82a
MsMarinaVasovic <129836995+MsMarinaVasovic@users.noreply.github.com>
2023-05-09 08:35:33 +0200

    Update ShoppingTest.java

--- src/test/java/ShoppingTest.java
+++ src/test/java/ShoppingTest.java
@@ -46,21 +46,23 @@ public class ShoppingTest extends BaseTest {
     }
 
     /**
-     * Testing adding one item to the shopping cart
+     * Testing adding two same items to the shopping cart
      * Steps:
      * 1.Go to home page
      * 2.Click on burger menu PRODAVNICA
      * 3.Click on option Sve kategorije
      * 4.Click on option Kursevi i radionice
      * 5. Click on button U KORPU on the selected product
-     * 6.Click on field for quantity enter (modal window)
+     * 6.Click on field for quantity enter (modal window) and enter 2
+     * 7.Click on close button from modal window
      *
      * Expected results:
      * Verify that icon on shopping cart shows 1
+     * Verify that number of same items in mini cart is 2
      */
     @Test
     public void testAddTwoSameItemsInShoppingCart() {
-        print("Testing adding one item to the shopping cart");
+        print("Testing adding two same items to the shopping cart");
         ChromeDriver driver = openChromeDriver();
         try {
             print("1.Go to home page");
@@ -75,13 +77,14 @@ public class ShoppingTest extends BaseTest {
             storePages.clickOnKurseviIRadionice();
             print("5. Click on button U KORPU on the selected product");
             storePages.clickOnUKorpuSTORITVEButton();
-            print("6.Click on field for quantity enter");
+            print("6.Click on field for quantity enter and enter 2");
             WebDriverWait wait = new WebDriverWait(driver, 30);
             WebElement kolicinaFieldFromModal = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='addItemQuantity']")));
             Actions action=new Actions(driver);
             action.moveToElement(kolicinaFieldFromModal).click().build().perform();
             kolicinaFieldFromModal.sendKeys(Keys.BACK_SPACE);
             kolicinaFieldFromModal.sendKeys("2");
+            print("7.Click on close button from modal window");
             action.moveToElement(storePages.closeButtonFromModalWindow).click().build().perform();
             driver.navigate().refresh();
             homePage.shoppingCart.click();
@@ -89,7 +92,7 @@ public class ShoppingTest extends BaseTest {
             String currentNumberOfSameItemsMiniCart = storePages.numberOfItemsInMiniCartWindow.getText();
 
             assert currentlNumberOfItemsInCart.equals("1") : "Wrong number of items in cart. Expected 1, actual: " + currentlNumberOfItemsInCart;
-            assert currentNumberOfSameItemsMiniCart.equals("2") : "Wrong number of items in cart. Expected 2, actual : " + currentNumberOfSameItemsMiniCart;
+            assert currentNumberOfSameItemsMiniCart.equals("2") : "Wrong number of same items in mini cart. Expected 2, actual : " + currentNumberOfSameItemsMiniCart;
 
         }finally {
             driver.quit();
